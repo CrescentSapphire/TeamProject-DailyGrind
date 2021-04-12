d5f8187e012aba3e16bd1e7ab30c29a4a261a3a5
CrescentSapphire <cresentsapphire@gmail.com>
2021-04-12 18:46:22 -0500

    Update to the templates

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.inventory, name='inventory'),
+    path('item/<int:pk>/', views.item_detail, name='item_detail'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import InventoryTable
 
 # Create your views here.
 def inventory(request):
-    return render(request, 'inventory/inventory.html')
+    inlist = InventoryTable.objects.all()
+    return render(request, 'inventory/inventory.html', {'list': inlist})
+
+def item_detail(request, pk):
+    post = get_object_or_404(InventoryTable, pk=pk)
+    return render(request, 'blog/post_detail.html', {'item': item})
